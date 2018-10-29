c31b5286d4bbfd56cb254d1cb9da45bf1482a9a1
paladox <paladox@users.noreply.github.com>
2018-10-29 00:26:15 +0000

    Add CURLOPT_TIMEOUT to RottenLinks class

--- includes/RottenLinks.php
+++ includes/RottenLinks.php
@@ -7,6 +7,7 @@ class RottenLinks {
 		curl_setopt( $ch, CURLOPT_USERAGENT, "RottenLink, MediaWiki extension (https://github.com/miraheze/RottenLinks), running on $wgServer" );
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $wgRottenLinksCurlTimeout );
+		curl_setopt( $ch, CURLOPT_TIMEOUT, $wgRottenLinksCurlTimeout );
 		curl_exec( $ch );
 		$result = curl_getinfo( $ch );
 		curl_close( $ch );
