38d270afb0bb8770155b31edfef5c531613ebf53
paladox <paladox@users.noreply.github.com>
2020-07-14 19:31:24 +0100

    Update RottenLinksHooks.php

--- includes/RottenLinksHooks.php
+++ includes/RottenLinksHooks.php
@@ -3,5 +3,8 @@ class RottenLinksHooks {
 	public static function fnRottenLinksSchemaUpdates( DatabaseUpdater $updater ) {
 		$updater->addExtensionTable( 'rottenlinks',
 			__DIR__ . '/../sql/rottenlinks.sql' );
+
+		$updater->modifyExtensionTable( 'rottenlinks',
+			__DIR__ . '/../sql/patches/patch- rl_externallink.sql' );
 	}
 }
