2964f2882e54eaf4a9bfd5065799bd7d758a8223
John Lewis <johnflewis93@gmail.com>
2020-09-08 19:52:26 +0100

    Increase key length + update extension.json

--- sql/rottenlinks.sql
+++ sql/rottenlinks.sql
@@ -1,5 +1,5 @@
 CREATE TABLE /*_*/rottenlinks (
-  `rl_externallink` VARCHAR(512) NOT NULL PRIMARY KEY,
+  `rl_externallink` VARCHAR(2048) NOT NULL PRIMARY KEY,
   `rl_respcode` INT UNSIGNED NOT NULL,
   `rl_pageusage` LONGTEXT NOT NULL
 ) /*$wgDBTableOptions*/;
