00906708e286281961271761621b0293aa3cb547
paladox <paladox@users.noreply.github.com>
2020-10-09 23:33:27 +0100

    Update RottenLinksHooks.php

--- includes/RottenLinksHooks.php
+++ includes/RottenLinksHooks.php
@@ -3,7 +3,7 @@ class RottenLinksHooks {
 	public static function fnRottenLinksSchemaUpdates( DatabaseUpdater $updater ) {
 		$updater->addExtensionTable( 'rottenlinks',
 			__DIR__ . '/../sql/rottenlinks.sql' );
-		$updater->modifyExtensionField( 'rottenlinks', 'rl_externallink',
+		$updater->addExtensionField( 'rottenlinks', 'rl_id',
 			__DIR__ . '/../sql/patches/patch-primary-key.sql');
 	}
 }
