caa3ce8292d40867969381289bfafc4e2aad61d4
paladox <paladox@users.noreply.github.com>
2020-10-09 23:34:13 +0100

    Update rottenlinks.sql

--- sql/rottenlinks.sql
+++ sql/rottenlinks.sql
@@ -1,5 +1,5 @@
 CREATE TABLE /*_*/rottenlinks (
-  `rl_id` AUTO_INCREMENT PRIMARY KEY,
+  `rl_id` INT AUTO_INCREMENT PRIMARY KEY,
   `rl_externallink` BLOB NOT NULL,
   `rl_respcode` INT UNSIGNED NOT NULL,
   `rl_pageusage` LONGTEXT NOT NULL
