2a3ad4fa59513e1310b9ee22d0f02374e73431c9
Pavel Astakhov <pastakhov@yandex.ru>
2021-02-15 15:35:36 +0600

    Comment out $updater add/modify calls in fnRottenLinksSchemaUpdates

--- includes/RottenLinksHooks.php
+++ includes/RottenLinksHooks.php
@@ -3,6 +3,7 @@ class RottenLinksHooks {
 	public static function fnRottenLinksSchemaUpdates( DatabaseUpdater $updater ) {
 		$updater->addExtensionTable( 'rottenlinks',
 			__DIR__ . '/../sql/rottenlinks.sql' );
+		/*
 		$updater->modifyExtensionField( 'rottenlinks', 'rl_externallink',
  			__DIR__ . '/../sql/patches/patch-primary-key.sql');
 		$updater->addExtensionField( 'rottenlinks', 'rl_id',
@@ -11,5 +12,6 @@ class RottenLinksHooks {
  			__DIR__ . '/../sql/patches/patch-modify-rl_id.sql');
 		$updater->modifyExtensionField( 'rottenlinks', 'rl_externallink',
  			__DIR__ . '/../sql/patches/convert-to-blob-rl_externallink.sql');
+		*/
 	}
 }
