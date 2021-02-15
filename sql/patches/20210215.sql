d2db896cc936f1890b7fac3a9eec53591770caa6
Pavel Astakhov <pastakhov@yandex.ru>
2021-02-15 18:52:27 +0600

    Set index lenght for rl_externallink and rl_pageusage columns

--- sql/patches/20210215.sql
+++ sql/patches/20210215.sql
@@ -3,8 +3,8 @@ BEGIN;
 ALTER TABLE /*$wgDBprefix*/rottenlinks
     MODIFY COLUMN rl_externallink BLOB NOT NULL;
 
-CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink);
+CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink(50));
 CREATE INDEX /*i*/rl_respcode ON /*_*/rottenlinks (rl_respcode);
-CREATE INDEX /*i*/rl_pageusage ON /*_*/rottenlinks (rl_pageusage);
+CREATE INDEX /*i*/rl_pageusage ON /*_*/rottenlinks (rl_pageusage(50));
 
 COMMIT;
--- sql/rottenlinks.sql
+++ sql/rottenlinks.sql
@@ -7,8 +7,8 @@ CREATE TABLE /*_*/rottenlinks (
   `rl_pageusage` LONGTEXT NOT NULL
 )/*$wgDBTableOptions*/;
 
-CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink);
+CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink(50));
 CREATE INDEX /*i*/rl_respcode ON /*_*/rottenlinks (rl_respcode);
-CREATE INDEX /*i*/rl_pageusage ON /*_*/rottenlinks (rl_pageusage);
+CREATE INDEX /*i*/rl_pageusage ON /*_*/rottenlinks (rl_pageusage(50));
 
 COMMIT;
