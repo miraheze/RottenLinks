66025b2699661ea70050307aae2ced78e6b7063c
R4356th <61620631+R4356th@users.noreply.github.com>
2021-06-03 21:18:24 +0600

    Only make the protocol lowercase

--- includes/RottenLinks.php
+++ includes/RottenLinks.php
@@ -7,9 +7,15 @@ class RottenLinks {
 		$services = MediaWikiServices::getInstance();
 
 		$config = $services->getConfigFactory()->makeConfig( 'rottenlinks' );
-
+		
+		// Make the protocol lowercase
+		$urlexp = explode( '://', $url);
+		$proto = strtolower( $urlexp[0] ) . '://';
+		$site = $urlexp[1]; 
+		$urltouse = $proto . $site;
+		
 		$request = $services->getHttpRequestFactory()->create(
-			strtolower( $url ), [ 
+			$urltouse ), [ 
 				'method' => 'HEAD', // return headers only
 				'timeout' => $config->get( 'RottenLinksCurlTimeout' ),
 				'userAgent' => 'RottenLinks, MediaWiki extension (https://github.com/miraheze/RottenLinks), running on ' . $config->get( 'Server' )
