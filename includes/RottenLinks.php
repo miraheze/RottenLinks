925f40d51a5a7d8e6fdcecdca98f05b9b90484f2
R4356th <61620631+R4356th@users.noreply.github.com>
2021-06-05 13:21:59 +0600

    Make Protocols Lowercase Before Sending Request (#34)

--- includes/RottenLinks.php
+++ includes/RottenLinks.php
@@ -7,9 +7,15 @@ class RottenLinks {
 		$services = MediaWikiServices::getInstance();
 
 		$config = $services->getConfigFactory()->makeConfig( 'rottenlinks' );
-
+		
+		// Make the protocol lowercase
+		$urlexp = explode( '://', $url);
+		$proto = strtolower( $urlexp[0] ) . '://';
+		$site = $urlexp[1]; 
+		$urlToUse = $proto . $site;
+		
 		$request = $services->getHttpRequestFactory()->create(
-			$url, [ 
+			$urlToUse, [ 
 				'method' => 'HEAD', // return headers only
 				'timeout' => $config->get( 'RottenLinksCurlTimeout' ),
 				'userAgent' => 'RottenLinks, MediaWiki extension (https://github.com/miraheze/RottenLinks), running on ' . $config->get( 'Server' )
