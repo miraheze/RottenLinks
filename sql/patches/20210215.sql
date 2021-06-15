a438fb6e68f5732fd60fd495902f2f954ac4b679
Pavel Astakhov <pastakhov@yandex.ru>
2021-06-15 10:22:01 +0600

    Fix sql patches for case when rottenlinks.rl_externallink is a primary key (#30)
    In some cases rottenlinks can have rl_externallink as a primary key, but in
    some cases it is not. In the first case the database updater throws
    SQL exception (it can't create primary key if it already exists)
    the removePrimaryKey stored procedure removes primary key in the
    rottenlinks table if it exists.

--- sql/patches/20210215.sql
+++ sql/patches/20210215.sql
@@ -1,8 +1,5 @@
 BEGIN;
 
-ALTER TABLE /*$wgDBprefix*/rottenlinks
-    MODIFY COLUMN rl_externallink BLOB NOT NULL;
-
 CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink(50));
 CREATE INDEX /*i*/rl_respcode ON /*_*/rottenlinks (rl_respcode);
 CREATE INDEX /*i*/rl_pageusage ON /*_*/rottenlinks (rl_pageusage(50));
