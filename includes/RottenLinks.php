7b77a5e3622dd321d5159e62ec3da75f94b2398d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-08-12 12:51:50 -0600

    Use MultiHttpClient

--- includes/RottenLinks.php
+++ includes/RottenLinks.php
@@ -14,15 +14,18 @@ class RottenLinks {
 		$site = $urlexp[1]; 
 		$urlToUse = $proto . $site;
 		
-		$request = $services->getHttpRequestFactory()->create(
-			$urlToUse, [ 
+		$request = $services->getHttpRequestFactory()->createMultiClient()
+			->run( [
+				'url' => $urlToUse,
 				'method' => 'HEAD', // return headers only
-				'timeout' => $config->get( 'RottenLinksCurlTimeout' ),
-				'userAgent' => 'RottenLinks, MediaWiki extension (https://github.com/miraheze/RottenLinks), running on ' . $config->get( 'Server' )
-			],
-			__METHOD__
-		)->execute();
+				'headers' => [ 
+					'user-agent' => 'RottenLinks, MediaWiki extension (https://github.com/miraheze/RottenLinks), running on ' . $config->get( 'Server' )
+				]
+			], [
+				'reqTimeout' => $config->get( 'RottenLinksCurlTimeout' )
+			]
+		);
 
-		return (int)$request->getStatusValue()->getValue();
+		return (int)$request['code'];
 	}
 }
