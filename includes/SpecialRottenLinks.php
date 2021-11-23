6a9d4b11efa5825eed7570d1fab0378f05ad7fa0
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-11-22 21:13:14 -0700

    Update SpecialRottenLinks.php

--- includes/SpecialRottenLinks.php
+++ includes/SpecialRottenLinks.php
@@ -12,7 +12,7 @@ class SpecialRottenLinks extends SpecialPage {
 		$showBad = $this->getRequest()->getText( 'showBad' );
 		$stats = $this->getRequest()->getText( 'stats' );
 
-		$pager = new RottenLinksPager( $showBad );
+		$pager = new RottenLinksPager( $this, $showBad );
 
 		$formDescriptor = [
 			'showBad' => [
@@ -37,7 +37,7 @@ class SpecialRottenLinks extends SpecialPage {
 		];
 
 		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext() );
-		$htmlForm->setSubmitCallback( [ $this, 'dummyProcess' ] )->setMethod( 'get' )->prepareForm()->show();
+		$htmlForm->setMethod( 'get' )->prepareForm()->displayForm( false );
 
 		if ( $stats ) {
 			$statForm = HTMLForm::factory( 'ooui', $this->showStatistics( $this->getContext() ), $this->getContext(), 'rottenlinks' );
@@ -46,10 +46,6 @@ class SpecialRottenLinks extends SpecialPage {
 		}
 
 		$this->getOutput()->addParserOutputContent( $pager->getFullOutput() );
-	}
-
-	public static function dummyProcess( $formData ) {
-		return false;
 	}
 
 	public static function showStatistics( IContextSource $context ) {
