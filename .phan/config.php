ced21dcfa8a343aa1a0046a8319401d11bcf7eae
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-06 13:04:21 -0700

    Enable more phan issues

--- .phan/config.php
+++ .phan/config.php
@@ -2,10 +2,6 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['suppress_issue_types'] = array_merge(
-	$cfg['suppress_issue_types'], []
-);
-
-$cfg['scalar_implicit_cast'] = true;
+$cfg['suppress_issue_types'] = [];
 
 return $cfg;
