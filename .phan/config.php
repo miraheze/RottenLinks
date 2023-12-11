044b0e1ac6ba4ce3a993c9aa7acd34e78db2d3c4
paladox <paladox@users.noreply.github.com>
2023-12-11 03:05:03 -0500

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -3,6 +3,7 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['suppress_issue_types'] = [
+	'PhanAccessMethodInternal',
 	'SecurityCheck-LikelyFalsePositive'
 ];
 
