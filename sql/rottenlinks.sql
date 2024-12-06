c64fa3f90adea387e0fd313c31eaa66e3139aedd
paladox <paladox@users.noreply.github.com>
2024-12-06 13:59:08 +0000

    Don't restrict rl_externallink index to 50

--- sql/rottenlinks.sql
+++ sql/rottenlinks.sql
@@ -6,7 +6,7 @@ CREATE TABLE /*_*/rottenlinks (
   `rl_respcode` INT UNSIGNED NOT NULL
 )/*$wgDBTableOptions*/;
 
-CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink(50));
+CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink);
 CREATE INDEX /*i*/rl_respcode ON /*_*/rottenlinks (rl_respcode);
 
 COMMIT;
