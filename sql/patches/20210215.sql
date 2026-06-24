a548534e17bb0f30a4d82aa27520976460cb708d
Skye <git@skyee.ee>
2026-06-24 04:41:30 +0300

    T15490: don't create index for rl_pageusage
    20210215.sql was never updated with/after a005bff/#73

--- sql/patches/20210215.sql
+++ sql/patches/20210215.sql
@@ -2,6 +2,5 @@ BEGIN;
 
 CREATE INDEX /*i*/rl_externallink ON /*_*/rottenlinks (rl_externallink(50));
 CREATE INDEX /*i*/rl_respcode ON /*_*/rottenlinks (rl_respcode);
-CREATE INDEX /*i*/rl_pageusage ON /*_*/rottenlinks (rl_pageusage(50));
 
 COMMIT;
